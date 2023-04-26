c016797f7f11e092dd3c2f7ad1e93153824d9699
Cristian <cristian.ciortea@student.unitbv.ro>
2023-04-26 11:13:21 +0300

    Removed additional spaces and added where needed

--- DataLayer/Entities/Grade.cs
+++ DataLayer/Entities/Grade.cs
@@ -5,9 +5,12 @@ namespace DataLayer.Entities;
 public class Grade : BaseEntity
 {
     public double Value { get; set; }
+
     public CourseType Course { get; set; }
+    
     public DateTime DateCreated { get; set; }
 
     public int StudentId { get; set; }
+    
     public Student Student { get; set; }
 }
